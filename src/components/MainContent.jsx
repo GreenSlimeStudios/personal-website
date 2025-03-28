ae612e6fa231648e20975c2a528269f55e64cf12
Max Berliński <maxber.gdynia@gmail.com>
2025-03-29 00:31:19 +0100

    brief midi-b description

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MainContent = () => {
     const womp = "bold";
     return (
-        <main className="mx-5 mt-5 mb-10 flex-row space-y-10">
+        <main className="mx-5 mt-5 mb-10 flex-row space-y-20">
             <div className="mb-8 flex gap-x-5 items-end">
                 <h1 className="text-5xl font-bold ">Karp<span className="bg-amber-500 text-transparent bg-clip-text">p</span>ortal</h1>
                 {/* <div className="p-1 bg-white rounded-lg"> */}
@@ -12,11 +12,12 @@ const MainContent = () => {
                 <p className="text-center text-[20px] text-amber-500 font-bold">DART</p>
             </div>
 
-            <div className="text-2xl border-amber-500 border-2 rounded-2xl p-4">
+            <div className="text-2xl border-amber-500 border-2 rounded-2xl p-4 space-y-5">
                 {/* <div className="p-1 bg-white float-left mr-4 rounded-2xl"> */}
                 {/* <img src="kpplogo.png" className="h-20 float-left mr-5"/> */}
                 {/* </div> */}
-                <p>Karpportal is a social-media platform I developed in Flutter and dart with firebase for the backend. The project aims to provide an app where pepople can post about their favourite stuff and communicate with each other. Some notable features of the app are, end to end encryption, group chats and high color customization.</p>
+                <p>Karpportal is a social-media platform I developed in Flutter and dart with firebase for the backend. The project aims to provide an app where people can post about their favourite stuff and communicate with each other. Some notable features of the app are, end to end encryption, group chats and highly customizable themes.</p>
+                <p>For complete list of features and more screenshots head to the <a className="text-amber-300 underline" target="blank_" href="https://github.com/GreenSlimeStudios/karpportal">github repo</a>.</p>
             </div>
             {/* <p className="bg-amber-400 text-transparent bg-clip-text"> womp womp</p> */}
 
@@ -47,10 +48,15 @@ const MainContent = () => {
 
             <div className="text-2xl border-indigo-500  border-2 rounded-2xl p-4 space-y-5">
                 <p >
-                    <span className="high-indigo">Midi-b</span>
+                    <span className="high-indigo">Midi-b</span> is a program that aims to visualise the notes played on a midi device like a piano in a nice visual way that is very customizable. it is the successor of my older version midi-recorder that is built from the ground up to have better performance and more functionality.
                 </p>
                 <p>
+                    The program written in Rust in the Bevy game engine, the predecessor was built also in rust but using the Nannou crate. Midi-b can visualize the notes live via a midi cable connection or replay a prerecorded midi file.
                 </p>
+                <p>
+                    I built this program because I do play the piano and I wanted to create something cool that would put my hobbies together.
+                </p>
+                <p>For a complete list of features and more video examples and screenshots head to the <a className="text-indigo-300 underline" target="blank_" href="https://github.com/GreenSlimeStudios/midi-b">github repo</a>.</p>
             </div>
 
             {/* <div className="h-0.5 mt-10 w-full bg-blue-500 rounded-full" /> */}
